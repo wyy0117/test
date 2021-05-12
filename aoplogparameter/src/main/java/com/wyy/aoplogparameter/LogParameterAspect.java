e0b7060d809859d7ae3f3cc88f6c78d4dfe8da10
wangyuyuan <wangyuyuan@xylink.com>
2021-05-12 18:55:06 +0800

    aop log parameter,support class

--- aoplogparameter/src/main/java/com/wyy/aoplogparameter/LogParameterAspect.java
+++ aoplogparameter/src/main/java/com/wyy/aoplogparameter/LogParameterAspect.java
@@ -1,6 +1,5 @@
 package com.wyy.aoplogparameter;
 
-
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -13,6 +12,9 @@ import java.util.Arrays;
 @Aspect
 public class LogParameterAspect {
 
+    /**
+     * 注解在类上时生效
+     */
     @Pointcut("@within(LogParameter)")
     public void logClass() {
     }
@@ -22,17 +24,9 @@ public class LogParameterAspect {
         doLog(joinPoint);
     }
 
-    private void doLog(JoinPoint joinPoint) {
-        Class<?> targetClass = joinPoint.getTarget().getClass();
-        String methodName = joinPoint.getSignature().getName();
-        //有缓存，不会每次new一个新的logger对象
-        Logger logger = LoggerFactory.getLogger(targetClass);
-        Object[] args = joinPoint.getArgs();
-        Arrays.stream(args).forEach(arg -> {
-            logger.debug("{}->{}", methodName, arg.toString());
-        });
-    }
-
+    /**
+     * 注解在方法上时生效
+     */
     @Pointcut("@annotation(LogParameter)")
     public void logMethod() {
     }
@@ -40,5 +34,24 @@ public class LogParameterAspect {
     @Before("logMethod()")
     public void logMethodParameter(JoinPoint joinPoint) {
         doLog(joinPoint);
+    }
+
+    private void doLog(JoinPoint joinPoint) {
+        Class<?> targetClass = joinPoint.getTarget().getClass();
+        String methodName = joinPoint.getSignature().getName();
+        //有缓存，不会每次new一个新的logger对象
+        Logger logger = LoggerFactory.getLogger(targetClass);
+        Object[] args = joinPoint.getArgs();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        /**
+         * 一个方法，log在一行中
+         */
+        Arrays.stream(args).forEach(arg -> {
+            stringBuilder.append(arg.toString());
+            stringBuilder.append(",");
+        });
+        String parameterStr = stringBuilder.substring(0, stringBuilder.length() - 1);
+        logger.debug("{}->{}", methodName, parameterStr);
     }
 }
